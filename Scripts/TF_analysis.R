49bc37da5baf9eb72e3e28613ba681ad469abcbb
Radhika Shaunak <rads.shaunak@gmail.com>
2026-06-08 16:39:18 +0100

    ciliated 2 cluster loop using ciliated 1 as template

--- Scripts/TF_analysis.R
+++ Scripts/TF_analysis.R
@@ -18,21 +18,6 @@ conserved_ChEA3 <- Reduce(intersect, list(
   as.character(final_ChEA3_mucociliated$TF)
 ))
 database_conserved <-intersect(conserved_ChEA3,conserved_JASPAR)
-#trialling with one TF
-practice <- "TFAP2A"
-practice_df <- data.frame()
-# JASPAR practice
-for (TF in practice){
-  target_genes <- final_JASPAR_TFs$symbol_DEG[final_JASPAR_TFs$symbol == TF]
-  #since DEGs stored as comma separated string
-  # Get the comma-separated string and split it into a character vector
-  raw_string <- final_JASPAR_TFs$DEG_symbol[final_JASPAR_TFs$symbol == TF]
-  target_genes <- unlist(strsplit(raw_string, ","))
-  target_genes <- trimws(target_genes)
-  #get expression data for target genes
-  exp_target <- FetchData(so_combined, vars = target_genes)
-  practice_df <- rbind(practice_df, exp_target)
-}
 #alternative approach - since the original aim is to review cell-type TFs, want to have cell-type specific TFs
 #want to go back a step actually, want to go to the non-aggregated version with a TF-gene observation in each row
 #for JASPAR: final_JASPAR_specific, for ChEA3:since final_ChEA3_TFs and final_ChEA3_unique_TF and final_ChEA3_notnull all have 5649 obs use final_ChEA3_specifi
@@ -42,13 +27,11 @@ colnames(ciliated1_gene_TF_JASPAR)[1] <- "gene"
 ciliated1_KS <- ciliated1_gene_TF_JASPAR %>% left_join(sig_ciliated1, by = "gene") %>%
   dplyr::select("gene","symbol", "score", "p_val_adj", "avg_log2FC")
 ciliated1_unique_TF <- unique(ciliated1_gene_TF_JASPAR$symbol)
-ciliated2_gene_TF_JASPAR <- final_JASPAR_specific[final_JASPAR_specific$cell_type == "ciliated 2",] %>% filter(score > 0)
-ciliated2_unique_TF <- unique(ciliated2_gene_TF_JASPAR$symbol)
 club_gene_TF_JASPAR <- final_JASPAR_specific[final_JASPAR_specific$cell_type == "club",] %>% filter(score > 0)
 club_unique_TF <- unique(club_gene_TF_JASPAR$symbol)
 goblet_gene_TF_JASPAR <- final_JASPAR_specific[final_JASPAR_specific$cell_type == "goblet",] %>% filter(score > 0)
 goblet_unique_TF <- unique(goblet_gene_TF_JASPAR$symbol)
-#start with ciliated 1 cluster
+#start with ciliated 1 cluster for JASPAR
 ciliated1_KS_results <- data.frame(matrix(NA, nrow = nrow(final_JASPAR_ciliated1), ncol = 2))
 colnames(ciliated1_KS_results) <- c("TF", "p_val")
 for (i in seq_along(ciliated1_unique_TF)) {
@@ -61,7 +44,36 @@ for (i in seq_along(ciliated1_unique_TF)) {
   non_target_TF <- ciliated1_KS[ciliated1_KS$symbol != TF,]
   #get a unique target DEG list for the second loop to refer to
   non_target_unique <- unique(non_target_TF$gene)
+  #adding results
   ciliated1_KS_results$TF[i] <- TF
   ciliated1_KS_results$p_val[i] <- ks.test(TF_target$avg_log2FC, non_target_TF$avg_log2FC)$p.val
   
 }
+#filtering for significant values
+ciliated1_KS_results_filtered <- ciliated1_KS_results %>% filter(p_val < 0.05)
+
+#ciliated 2 cluster for JASPAR
+ciliated2_gene_TF_JASPAR <- final_JASPAR_specific[final_JASPAR_specific$cell_type == "ciliated 2",] %>% filter(score > 0)
+ciliated2_unique_TF <- unique(ciliated2_gene_TF_JASPAR$symbol)
+colnames(ciliated2_gene_TF_JASPAR)[1] <- "gene"
+ciliated2_KS <- ciliated2_gene_TF_JASPAR %>% left_join(sig_ciliated2, by = "gene") %>%
+  dplyr::select("gene","symbol", "score", "p_val_adj", "avg_log2FC")
+ciliated2_KS_results <- data.frame(matrix(NA, nrow = nrow(final_JASPAR_ciliated2), ncol = 2))
+colnames(ciliated2_KS_results) <- c("TF", "p_val")
+for (i in seq_along(ciliated2_unique_TF)) {
+  #subset target DEGs
+  TF <- ciliated2_unique_TF[i]
+  TF_target <- ciliated2_KS[ciliated2_KS$symbol == TF,]
+  #get a unique target DEG list for the second loop to refer to
+  target_unique <- unique(TF_target$gene)
+  #subset non-target DEGs
+  non_target_TF <- ciliated2_KS[ciliated2_KS$symbol != TF,]
+  #get a unique target DEG list for the second loop to refer to
+  non_target_unique <- unique(non_target_TF$gene)
+  ciliated2_KS_results$TF[i] <- TF
+  ciliated2_KS_results$p_val[i] <- ks.test(TF_target$avg_log2FC, non_target_TF$avg_log2FC)$p.val
+  
+}
+ciliated2_KS_results_filtered <- ciliated2_KS_results %>% filter(p_val < 0.05)
+
+#club
