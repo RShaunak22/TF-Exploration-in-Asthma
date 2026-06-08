394c546853169b7a3de1701331d64e27f272193b
Radhika Shaunak <rads.shaunak@gmail.com>
2026-06-08 11:59:53 +0100

    developing K-S loop for ciliated 1 cluster

--- Scripts/TF_analysis.R
+++ Scripts/TF_analysis.R
@@ -1,4 +1,7 @@
 #this script is for determining the number of TFs conserved across cell-types and specific to cell-types
+#dependencies
+library(dplyr)
+library(stats)
 #JASPAR across all cell types with TFs recorded
 conserved_JASPAR <- Reduce(intersect, list(
   final_JASPAR_ciliated1$symbol,
@@ -35,6 +38,9 @@ for (TF in practice){
 #for JASPAR: final_JASPAR_specific, for ChEA3:since final_ChEA3_TFs and final_ChEA3_unique_TF and final_ChEA3_notnull all have 5649 obs use final_ChEA3_specifi
 #JASPAR
 ciliated1_gene_TF_JASPAR <- final_JASPAR_specific[final_JASPAR_specific$cell_type == "ciliated 1",] %>% filter(score > 0)
+colnames(ciliated1_gene_TF_JASPAR)[1] <- "gene"
+ciliated1_KS <- ciliated1_gene_TF_JASPAR %>% left_join(sig_ciliated1, by = "gene") %>%
+  dplyr::select("gene","symbol", "score", "p_val_adj", "avg_log2FC")
 ciliated1_unique_TF <- unique(ciliated1_gene_TF_JASPAR$symbol)
 ciliated2_gene_TF_JASPAR <- final_JASPAR_specific[final_JASPAR_specific$cell_type == "ciliated 2",] %>% filter(score > 0)
 ciliated2_unique_TF <- unique(ciliated2_gene_TF_JASPAR$symbol)
@@ -45,17 +51,12 @@ goblet_unique_TF <- unique(goblet_gene_TF_JASPAR$symbol)
 #start with ciliated 1 cluster
 for (TF in ciliated1_unique_TF) {
   #subset target DEGs
-  TF_target <- ciliated1_gene_TF_JASPAR[ciliated1_gene_TF_JASPAR$symbol == TF,]
+  TF_target <- ciliated1_KS[ciliated1_KS$symbol == TF,]
   #get a unique target DEG list for the second loop to refer to
-  target_unique <- unique(TF_target$symbol_DEG)
-    for (DEG in target_unique){
-    
-    }
+  target_unique <- unique(TF_target$gene)
   #subset non-target DEGs
-  non-target_TF <- final_JASPAR_specific[final_JASPAR_specific$symbol != TF,]
+  non_target_TF <- ciliated1_KS[ciliated1_KS$symbol != TF,]
   #get a unique target DEG list for the second loop to refer to
-  non_target_unique <- unique(non_target_TF$symbol_DEG)
-    for (gene in non_target_unique) {
-      
-    }
+  non_target_unique <- unique(non_target_TF$gene)
+  ks.test(TF_target$avg_log2FC, non_target_TF$avg_log2FC)
 }
